63a4951ffc15d301342f931685c72a3d74c08552
gronki <gronki@gmail.com>
2016-03-16 02:32:08 +0100

    Update sswstart.sh

--- sswstart.sh
+++ sswstart.sh
@@ -1,7 +1,6 @@
 #!/bin/csh
 setenv SSW /usr/local/ssw
 setenv SSW_INSTR "gen vso ontology aia"
-# often used: gen vso ontology aia goes_sxig12 goes_sxig13 chianti xray hessi spex
 source $SSW/gen/setup/setup.ssw /loud
 
 if ! $?EXEC then
